bfa9439fd7d618c4bc63c49dbfc4a504e26f38b8
DmitryDrynov <dmitry.drynov@gmail.com>
2021-09-06 00:32:13 +0300

    add layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,30 @@
 import type { AppProps } from 'next/app'
+import { NextPage } from 'next'
+import Head from 'next/head'
+import React, { ReactElement, ReactNode } from 'react'
+import PublicLayout from '@components/layouts/PublicLayout'
 import 'tailwindcss/tailwind.css'
 import '@styles/globals.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+export type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout =
+        Component.getLayout ?? ((page) => <PublicLayout>{page}</PublicLayout>)
+
+    return getLayout(
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 export default MyApp
