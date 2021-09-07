7ba0e22295c177c21d8395d223886d2278183d4f
DmitryDrynov <dmitry.drynov@gmail.com>
2021-09-07 23:48:51 +0300

    fixed last merge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app'
-<<<<<<< HEAD
 import React, { ReactElement, ReactNode, useEffect } from 'react'
 import PublicLayout from 'src/layouts/PublicLayout'
 import { NextPage } from 'next'
@@ -10,14 +9,6 @@ import '@styles/globals.less'
 import { graphqlClient } from 'src/services/graphql/client';
 import { getCookie } from 'src/helpers/cookie';
 import { setLogin } from 'src/store/reducers/auth';
-=======
-import { NextPage } from 'next'
-import Head from 'next/head'
-import React, { ReactElement, ReactNode } from 'react'
-import PublicLayout from '@components/layouts/PublicLayout'
-import 'tailwindcss/tailwind.css'
-import '@styles/globals.scss'
->>>>>>> bfa9439fd7d618c4bc63c49dbfc4a504e26f38b8
 
 export type NextPageWithLayout = NextPage & {
     getLayout?: (page: ReactElement) => ReactNode
@@ -28,7 +19,6 @@ type AppPropsWithLayout = AppProps & {
 }
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-<<<<<<< HEAD
     // const dispatch = useDispatch();
     const getLayout = Component.getLayout || ((page) => <PublicLayout>{page}</PublicLayout>)
 
@@ -48,18 +38,5 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
             </UrqlProvider>
         </StoreProvider>
     );
-=======
-    const getLayout =
-        Component.getLayout ?? ((page) => <PublicLayout>{page}</PublicLayout>)
-
-    return getLayout(
-        <>
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    )
->>>>>>> bfa9439fd7d618c4bc63c49dbfc4a504e26f38b8
 }
 export default MyApp
