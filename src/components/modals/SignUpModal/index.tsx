533b5cf8bbe1e1626530c9c1abf7b73097e327ef
Dmitry Drynov <dmitry.drynov@gmail.com>
2021-09-21 20:13:24 +0300

    fixed lint warnings

--- src/components/modals/SignUpModal/index.tsx
+++ src/components/modals/SignUpModal/index.tsx
@@ -8,8 +8,8 @@ import { useMutation } from 'urql';
 import styles from './SignUpModal.module.less';
 
 type SignUpModalArgs = {
-    visible: boolean;
-    onClose: () => void;
+	visible: boolean;
+	onClose: () => void;
 }
 
 // type SignUpRequest = {
@@ -21,159 +21,159 @@ type SignUpModalArgs = {
 // }
 
 const SignUpModal: FC<SignUpModalArgs> = ({ visible, onClose }) => {
-    const [form] = Form.useForm();
-    // const dispatch = useDispatch();
-    const [step, setStep] = useState('email');
-    const [sendConfirmCodeResponse] = useMutation(sendConfirmCodeMutation);
-    // const [authorizedResponse, authorize] = useMutation(authorizeMutation);
+	const [form] = Form.useForm();
+	// const dispatch = useDispatch();
+	const [step, setStep] = useState('email');
+	const [sendConfirmCodeResponse] = useMutation(sendConfirmCodeMutation);
+	// const [authorizedResponse, authorize] = useMutation(authorizeMutation);
 
-    useEffect(() => {
-        form.resetFields();
-        setStep('email');
-    }, [form])
+	useEffect(() => {
+		form.resetFields();
+		setStep('email');
+	}, [form])
 
-    const handleOk = () => {
-        form
-            .validateFields()
-            .then(async (/*values: SignUpRequest*/) => {
-                try {
+	const handleOk = () => {
+		form
+			.validateFields()
+			.then(async (/*values: SignUpRequest*/) => {
+				try {
 
-                    if (step === 'email') {
-                        // const { data, error } = await sendConfirmCode({
-                        //     email: values.email,
-                        // });
+					if (step === 'email') {
+						// const { data, error } = await sendConfirmCode({
+						//     email: values.email,
+						// });
 
-                        // if (error) {
-                        //     message.error(error.message);
-                        //     return;
-                        // }
+						// if (error) {
+						//     message.error(error.message);
+						//     return;
+						// }
 
-                        // if (data.sendConfirmCode.code) {
-                        //     message.error(data.register.message);
-                        //     return;
-                        // }
+						// if (data.sendConfirmCode.code) {
+						//     message.error(data.register.message);
+						//     return;
+						// }
 
-                        setStep('password');
-                    }
+						setStep('password');
+					}
 
-                    if (step === 'password') {
+					if (step === 'password') {
 
-                        // const { data, error } = await registerMutation({
-                        //     ...values,
-                        // });
+						// const { data, error } = await registerMutation({
+						//     ...values,
+						// });
 
-                        // if (error) {
-                        //     message.error(error.message);
-                        //     return;
-                        // }
+						// if (error) {
+						//     message.error(error.message);
+						//     return;
+						// }
 
-                        // if (data.sendConfirmCode.code) {
-                        //     message.error(data.register.message);
-                        //     return;
-                        // }
+						// if (data.sendConfirmCode.code) {
+						//     message.error(data.register.message);
+						//     return;
+						// }
 
-                        message.success('Your are welcome!');
-                        onClose();
-                    }
-                } catch (e: any) {
-                    message.error(e.message);
-                }
-            })
-            .catch(info => {
-                console.log('Validate Failed:', info);
-            });
-    }
+						message.success('Your are welcome!');
+						onClose();
+					}
+				} catch (e: any) {
+					message.error(e.message);
+				}
+			})
+			.catch(info => {
+				console.log('Validate Failed:', info);
+			});
+	}
 
-    const handleCancel = () => {
-        onClose();
-    }
+	const handleCancel = () => {
+		onClose();
+	}
 
-    return (
-        <Modal
-            title={null}
-            visible={visible}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            width={450}
-            centered={true}
-            maskClosable={false}
-            className={styles.modal}
-            footer={[
-                <Button
-                    key="submit"
-                    type="primary"
-                    size="large"
-                    loading={sendConfirmCodeResponse.fetching}
-                    onClick={handleOk}
-                    className={styles.submitBtn}
-                >
-                    {step === 'email' ? 'Continue' : 'Sign Up'}
-                </Button>,
-            ]}
-        >
-            <Row justify="center">
-                <div className={styles.imageContainer}>
-                    <Image
-                        src={SignModalIllustration}
-                        alt="image for sign up form"
-                    />
-                </div>
-                <h2 className={styles.title}>Get Connect to the best Mentors</h2>
-                <p className={styles.subtitle}>Easy way to connect to mentor and get
-                    advise solution of design. </p>
-                <Form
-                    className={styles.form}
-                    form={form}
-                    layout="vertical"
-                    name="form_in_modal"
-                    initialValues={{ modifier: 'public' }}
-                    requiredMark={false}
-                >
-                    <Form.Item
-                        name="email"
-                        rules={[{ type: 'email', required: true, message: 'Please input the email!' }]}
-                    >
-                        <Input placeholder="Email" />
-                    </Form.Item>
-                    {step === 'password' &&
-                        <>
-                            <Row gutter={16}>
-                                <Col span={12}>
-                                    <Form.Item
-                                        name="firstName"
-                                        rules={[{ required: true, message: 'Please input the first name!' }]}
-                                    >
-                                        <Input placeholder="First name" />
-                                    </Form.Item>
-                                </Col>
-                                <Col span={12}>
-                                    <Form.Item
-                                        name="lastName"
-                                        rules={[{ required: true, message: 'Please input the last name!' }]}
-                                    >
-                                        <Input placeholder="Last name" />
-                                    </Form.Item>
-                                </Col>
-                            </Row>
-                            <Form.Item
-                                name="password"
-                                label={
-                                    <Row style={{ width: '100%' }} justify="end">
-                                        <Col>
-                                            <Button type="link">Resend a code</Button>
-                                        </Col>
-                                    </Row>
-                                }
-                                rules={[{ required: true, message: 'Please input the password!' }]}
-                            >
-                                <Input.Password placeholder="The code sent to your email" />
-                            </Form.Item>
-                        </>
-                    }
-                </Form>
-            </Row>
-        </Modal>
-    );
+	return (
+		<Modal
+			title={null}
+			visible={visible}
+			onOk={handleOk}
+			onCancel={handleCancel}
+			width={450}
+			centered={true}
+			maskClosable={false}
+			className={styles.modal}
+			footer={[
+				<Button
+					key="submit"
+					type="primary"
+					size="large"
+					loading={sendConfirmCodeResponse.fetching}
+					onClick={handleOk}
+					className={styles.submitBtn}
+				>
+					{step === 'email' ? 'Continue' : 'Sign Up'}
+				</Button>,
+			]}
+		>
+			<Row justify="center">
+				<div className={styles.imageContainer}>
+					<Image
+						src={SignModalIllustration}
+						alt="image for sign up form"
+					/>
+				</div>
+				<h2 className={styles.title}>Get Connect to the best Mentors</h2>
+				<p className={styles.subtitle}>Easy way to connect to mentor and get
+					advise solution of design. </p>
+				<Form
+					className={styles.form}
+					form={form}
+					layout="vertical"
+					name="form_in_modal"
+					initialValues={{ modifier: 'public' }}
+					requiredMark={false}
+				>
+					<Form.Item
+						name="email"
+						rules={[{ type: 'email', required: true, message: 'Please input the email!' }]}
+					>
+						<Input placeholder="Email" />
+					</Form.Item>
+					{step === 'password' &&
+						<>
+							<Row gutter={16}>
+								<Col span={12}>
+									<Form.Item
+										name="firstName"
+										rules={[{ required: true, message: 'Please input the first name!' }]}
+									>
+										<Input placeholder="First name" />
+									</Form.Item>
+								</Col>
+								<Col span={12}>
+									<Form.Item
+										name="lastName"
+										rules={[{ required: true, message: 'Please input the last name!' }]}
+									>
+										<Input placeholder="Last name" />
+									</Form.Item>
+								</Col>
+							</Row>
+							<Form.Item
+								name="password"
+								label={
+									<Row style={{ width: '100%' }} justify="end">
+										<Col>
+											<Button type="link">Resend a code</Button>
+										</Col>
+									</Row>
+								}
+								rules={[{ required: true, message: 'Please input the password!' }]}
+							>
+								<Input.Password placeholder="The code sent to your email" />
+							</Form.Item>
+						</>
+					}
+				</Form>
+			</Row>
+		</Modal>
+	);
 }
 
 export default SignUpModal;
