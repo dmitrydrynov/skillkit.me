cb9454d3db3ea9b01953f251fbca45966d5836ae
Dmitry Drynov <dmitry.drynov@gmail.com>
2022-06-14 00:05:10 +0300

    Fix share page, Add Google Tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ import ProgressBar from '@badrap/bar-of-progress';
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import Router, { useRouter } from 'next/router';
+import Script from 'next/script';
 import { Provider as StoreProvider, useDispatch, useSelector } from 'react-redux';
 import { Provider as UrqlProvider, useQuery } from 'urql';
 
@@ -113,7 +114,20 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 			<StoreProvider store={store}>
 				<UrqlProvider value={graphqlClient}>
 					<AuthProvider>
-						<div style={{ opacity: loading ? 0 : 1 }}>{getLayout(<Component {...pageProps} />)}</div>
+						<div style={{ opacity: loading ? 0 : 1 }}>
+							{getLayout(
+								<>
+									<Script id="google-tag-manager" strategy="afterInteractive">
+										{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K9N6B7P');`}
+									</Script>
+									<Component {...pageProps} />
+								</>,
+							)}
+						</div>
 					</AuthProvider>
 				</UrqlProvider>
 			</StoreProvider>
