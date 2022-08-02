456c2842f8496fe0946af9f3f443af15c6a34cc7
Dmitry Drynov <dmitry.drynov@gmail.com>
2022-08-03 00:43:46 +0300

    Added ghost env variables

--- src/services/ghost/posts.ts
+++ src/services/ghost/posts.ts
@@ -2,8 +2,8 @@ import GhostContentAPI from '@tryghost/content-api';
 
 // Create API instance with site credentials
 const api = new GhostContentAPI({
-	url: 'http://skillkit_ghost:2368',
-	key: '7a478d6c96b42255ae2a3280ed',
+	url: process.env.NEXT_PUBLIC_GHOST_URL,
+	key: process.env.NEXT_PUBLIC_GHOST_KEY,
 	version: 'v5.0',
 });
 
