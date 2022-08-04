af8b55950047d44e0b5d920d22bd1a75cbff501d
Dmitry Drynov <dmitry.drynov@gmail.com>
2022-08-04 21:34:59 +0300

    Fix eslint problems

--- src/pages/user/skills/index.tsx
+++ src/pages/user/skills/index.tsx
@@ -66,7 +66,8 @@ const ProfilePage: NextPageWithLayout = () => {
 				if (key === 'share' && recordId) {
 					// console.log('share skill', recordId);
 				}
-			}}>
+			}}
+		>
 			{screens.sm === false && (
 				<>
 					<Menu.Item key="edit">
@@ -138,7 +139,8 @@ const ProfilePage: NextPageWithLayout = () => {
 							icon={<PlusOutlined />}
 							onClick={() => {
 								setVisibleAddUserSkillModal(true);
-							}}>
+							}}
+						>
 							Add first skill
 						</Button>
 					</Space>
@@ -157,7 +159,8 @@ const ProfilePage: NextPageWithLayout = () => {
 								icon={<PlusOutlined />}
 								onClick={() => {
 									setVisibleAddUserSkillModal(true);
-								}}>
+								}}
+							>
 								Add skill
 							</Button>,
 						]}
@@ -169,7 +172,8 @@ const ProfilePage: NextPageWithLayout = () => {
 							dataSource={userSkills.data?.userSkills}
 							loading={userSkills.fetching}
 							pagination={false}
-							size="middle">
+							size="middle"
+						>
 							<Table.Column
 								title="I can"
 								dataIndex={['skill', 'name']}
@@ -235,7 +239,8 @@ const ProfilePage: NextPageWithLayout = () => {
 											type="text"
 											size="small"
 											icon={<EditTwoTone twoToneColor="#eb2f96" />}
-											onClick={() => router.push(`/user/skill/${record.id}/editor`)}>
+											onClick={() => router.push(`/user/skill/${record.id}/editor`)}
+										>
 											Details
 										</Button>
 									</>
@@ -253,7 +258,8 @@ const ProfilePage: NextPageWithLayout = () => {
 											onClick={(e) => {
 												e.preventDefault();
 												e.stopPropagation();
-											}}>
+											}}
+										>
 											<HiDotsVertical />
 										</Button>
 									</Dropdown>
