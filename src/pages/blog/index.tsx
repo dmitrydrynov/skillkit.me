cb30249f1ee8fb97d4675d3f04ce0475bac7fd16
Dmitry Drynov <dmitry.drynov@gmail.com>
2022-08-11 19:51:22 +0300

    Add meta for blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,11 +1,40 @@
 import { FC } from 'react';
 import { fetchPosts } from '@models/post';
 import { Card, Col, Row } from 'antd';
+import Head from 'next/head';
 import Link from 'next/link';
 import styles from './style.module.less';
 
 const PostsPage: FC = (props: any) => (
 	<>
+		<Head>
+			<title>Skillkit.me Blog - The best tips, insights and career advice</title>
+			<meta
+				name="description"
+				content="Welcome to the Skillkit.me Blog! Here you&#39;ll find the best information about resume and cover letter writing, job search and career advice. Read our blog articles to help you build a successful career."
+			/>
+			<meta property="og:title" content="killkit.me Blog - The best tips, insights and career advice" />
+			<meta
+				property="og:description"
+				content="Welcome to the Skillkit.me Blog! Here you&#39;ll find the best information about resume and cover letter writing, job search and career advice. Read our blog articles to help you build a successful career."
+			/>
+			<meta property="og:url" content={process.env.NEXT_PUBLIC_APP_URL + '/blog'} />
+			<meta property="og:article:author" content="aaa" />
+			<meta property="og:type" content="website" />
+			<meta
+				property="og:image"
+				content="https://cdn.sanity.io/images/n0lpvrpe/prod/c3b822ec8b441294d8e1835406f367e795d6dbfc-634x521.png"
+			/>
+			<meta
+				name="twitter:images"
+				content="https://cdn.sanity.io/images/n0lpvrpe/prod/c3b822ec8b441294d8e1835406f367e795d6dbfc-634x521.png"
+			/>
+			<meta name="twitter:title" content="killkit.me Blog - The best tips, insights and career advice" />
+			<meta
+				name="twitter:description"
+				content="Welcome to the Skillkit.me Blog! Here you&#39;ll find the best information about resume and cover letter writing, job search and career advice. Read our blog articles to help you build a successful career."
+			/>
+		</Head>
 		<h1 className={styles.header}>Our Blog</h1>
 		<Row gutter={16}>
 			{props.posts?.map((post) => (
